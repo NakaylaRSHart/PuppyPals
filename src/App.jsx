7b53232bbba9cfbf99f0ba37a6198ceb8c7d85c0
NakaylaRSHart <nakaylahart@DESKTOP-SJTLOE9>
2024-03-25 19:54:45 -0600

    Finished the card for the click on puppies. Added styling and a hover effect for the list of puppies.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,39 @@
-import './App.css'
-import {puppyList} from './data.js'
-import { useState } from 'react'
+import './App.css';
+import { puppyList } from './data.js';
+import { useState } from 'react';
+
 
 function App() {
-  const [puppies, setPuppies] = useState(puppyList)
-  console.log(puppies);
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
 
   return (
     <>
-    <div className='App'>
-    { 
-        puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
-        })
-      }
-    </div>
+      <div className='App'>
+        <h1>Puppy Pals</h1>
 
+        {puppies.map((puppy) => {
+          return <p onClick={() => setFeatPupId(puppy.id)} key={puppy.id}>{puppy.name}</p>;
+        })}
+
+        {featPupId && <p> {} </p>}
+
+        {featuredPup && (
+          <div id='puppyClicked'>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Puppy Id: {featPupId}</li>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
